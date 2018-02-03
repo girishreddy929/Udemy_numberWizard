bcae43cfe4be653e57578ef95b19f91a88e9f3d0
giri929 <reddygirish929@gmail.com>
2018-02-03 22:41:57 +0530

    Adding Intial Game

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizards : MonoBehaviour {
+
+	int max = 1000;
+	int min = 1;
+	int guess=500;
+
+	void Start () {
+		
+		print ("Welcome to Number Wizards ");
+		print ("Select a number and don't tell me!");
+
+
+
+		print ("Select a Number in less than :     " + max);
+		print ("Select a number in more than :     " + min);
+		max += 1;
+		print ("Is your number more than or less than "+ guess);
+		print ("up arrow = higher,down arrow = lower and return = equal");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			min = guess;
+			guess = (min + max) / 2;
+			print ("Higher or lower than " + guess);
+
+		}
+		else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			max = guess;
+			guess = (min + max) / 2;
+			print ("Higher or lower than " + guess);
+		}
+		else if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("I won!");
+		}
+
+	}
+}
